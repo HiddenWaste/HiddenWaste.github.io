c85bfe6c8afa4e6b02134c466ed6dc604f9d50b4
HiddenWaste <cartergordon13@gmail.com>
2024-03-25 21:00:37 -0500

    Transition to Gatsby
    Pivoting to making this into a gatsby based website/portfolio for projects, employers, and various other capacities

--- Code/Javascript.js
+++
@@ -1,8 +0,0 @@
-function ShowHideItem(itemId) {
-    const x = document.getElementById(String(itemId))
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
--- Code/Minigames/CrackTheWord.py
+++
@@ -1,7 +0,0 @@
-#Crack the Word
-#Introduction to program
-print("Hello I'm Kokoro! Imma challenge you to figure out what this wod is!")
-
-#function to scramble word
-def Scramble(word):
-    
