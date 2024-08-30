e7bf03cfe35066e0a918463882dee339fe91f841
HiddenWaste <cartergordon13@gmail.com>
2024-08-29 21:19:08 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' instead of 'react-dom'
 import App from './App';
 
-// Rendering the App component into the root element
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a root element using createRoot and render the App component
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
